e6d023fdfc74884d541c85bdc7cb010c516cf962
mmarentette <melrosemarentette@gmail.com>
2023-12-11 14:08:53 -0500

    Create index view for dresses model

--- main_app/urls.py
+++ main_app/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('about/', views.about, name='about'),
+    path('dresses/', views.dresses_index, name='index'),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,8 +1,20 @@
 from django.shortcuts import render
 
+dresses = [
+    {'style': 'Mira', 'collection': 'Eternal', 'designer': 'Daalarna', 'size': 0},
+    {'style': 'Minty', 'collection': 'Provence', 'designer': 'Tom Sebastien', 'size': 0}
+]
+
 # Create your views here.
 def home(request):
     return render(request, 'home.html')
 
 def about(request):
     return render(request, 'about.html')
+
+def dresses_index(request):
+    # We are namespacing the index.html template by putting it in a new tempates/dresses folder for organization purposes
+    # We pass a dictionary as a third positional argument in render; we can access the values in the template by using the key 'dresses'
+    return render(request, 'dresses/index.html', {
+        'dresses': dresses
+    })
