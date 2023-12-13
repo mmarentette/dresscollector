02395dbecd04f7f2b3d9c13615ac9d1fddea4a72
mmarentette <melrosemarentette@gmail.com>
2023-12-12 21:21:22 -0500

    Code update and delete functionality for dress

--- main_app/urls.py
+++ main_app/urls.py
@@ -8,4 +8,7 @@ urlpatterns = [
     path('dresses/<int:dress_id>/', views.dresses_detail, name='detail'),
     # Notice that we don't have to put the create view above the detail view because, unlike Express, Django won't match a route unless the second segment has an integer (and therefore it won't match 'create' to the integer above)
     path('dresses/create/', views.DressCreate.as_view(), name='dresses_create'),
+    # Note: we must use pk as named parameter instead of dress_id; this is convention for CBVs that work with model instances
+    path('dresses/<int:pk>/update/', views.DressUpdate.as_view(), name='dresses_update'),
+    path('dresses/<int:pk>/delete/', views.DressDelete.as_view(), name='dresses_delete'),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic.edit import CreateView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 
 from .models import Dress
 
@@ -27,3 +27,11 @@ def dresses_detail(request, dress_id):
 class DressCreate(CreateView):
     model = Dress
     fields = '__all__' # fields can be used to limit or change the ordering of attributes from the model that are generated in the ModelForm
+
+class DressUpdate(UpdateView):
+    model = Dress
+    fields = '__all__'
+
+class DressDelete(DeleteView):
+    model = Dress
+    success_url = '/dresses'
